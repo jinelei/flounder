be54106cf3c713937124b3de42a4f5d44daf3da9
jinelei <jinelei@163.com>
2019-02-19 15:04:17 +0800

    分离main到click-counter

---
+++ lib/view/click-counter.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class ClickCounter extends StatefulWidget {
+
+  @override
+  _ClickCounterState createState() => _ClickCounterState()
+}
+
+class _ClickCounterState extends State<ClickCounter> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("click counter"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme
+                  .of(context)
+                  .textTheme
+                  .display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+}
+
